da1e92374118daf4d5a28536cae32d0274a81eb4
dcdcdcdc221 <email>
2024-07-19 16:53:10 +0800

    新增es配置

---
+++ src/main/java/com/deng/springbootinit/controller/front/SearchController.java
@@ -0,0 +1,12 @@
+package com.deng.springbootinit.controller.front;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 前台-搜索API
+ */
+@RestController
+@RequestMapping("/search")
+public class SearchController {
+}
---
+++ src/main/java/com/deng/springbootinit/service/SearchService.java
@@ -0,0 +1,9 @@
+package com.deng.springbootinit.service;
+
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * 搜索服务类
+ */
+public interface SearchService extends ElasticsearchRepository {
+}
